c7fc3aba38ff6f18ac9e742f01cc8914f291a7a0
thesirtaylor <ajayiolatunji17@gmail.com>
2021-01-29 04:08:57 +0100

    project completed

---
+++ controller/exchange-rate.js
@@ -0,0 +1,24 @@
+const { default: axios } = require("axios");
+let HTTP_STATUS = require("../helpers/response/httpStatusCode");
+
+module.exports = {
+  rate: async (req, res) => {
+    try {
+      let query = req.query.currency;
+      nquery = query.split(",");
+      let { data } = await axios.get("https://api.exchangeratesapi.io/latest", {
+        params: { base: req.query.base },
+      });
+      let sas = {};
+      await nquery.forEach((n) => (sas[n] = data.rates[n]));
+      console.log(data);
+      return res.status(HTTP_STATUS.FOUND).json({
+        results: { base: req.query.base, date: data.date, rates: sas },
+      });
+    } catch (error) {
+      // console.log(error);
+      console.log(error);
+      return res.status(HTTP_STATUS.BAD_REQUEST).json(error.message);
+    }
+  },
+};
--- helpers/routes.js
+++ helpers/routes.js
@@ -1,11 +1,14 @@
 "use strict";
-
+let rate = require("../controller/exchange-rate");
 var express = require("express"),
   router = express.Router();
 
-
 module.exports = function (app) {
- 
-
+  router.get("/", (req, res) => {
+    res.send(`
+    Hey, check out current exchange rate in multiple currencies
+    `);
+  });
+  router.get("/api/rates", rate.rate);
   app.use(router);
 };
